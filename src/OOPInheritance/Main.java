12fac5a34d30e033e36b3b4ef72ed43b67822f9f
KlausKollerKroeff <kroeffk@icloud.com>
2024-06-01 13:23:13 -0300

    fix: OOPInheritance

--- src/OOPInheritance/Main.java
+++ src/OOPInheritance/Main.java
@@ -3,6 +3,9 @@ package OOPInheritance;
 public class Main {
     public static void main (String[] args){
 
+        // inheritance = 	the process where one class acquires,
+        //					the attributes and methods of another.
+
         //instanciando
         Car myCar = new Car();
         Bycicle myBycicle = new Bycicle();
