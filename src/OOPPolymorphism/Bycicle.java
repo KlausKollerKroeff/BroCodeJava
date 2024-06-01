13b705ef3782d2bf9e6fddcb71580f9e2c59f30f
KlausKollerKroeff <kroeffk@icloud.com>
2024-06-01 13:23:41 -0300

    new: OOPPolymorphism

---
+++ src/OOPPolymorphism/Bycicle.java
@@ -0,0 +1,8 @@
+package OOPPolymorphism;
+
+public class Bycicle extends Vehicle{
+    @Override
+    public void go(){
+        System.out.println("The bycicle begins moving");
+    }
+}
---
+++ src/OOPPolymorphism/Vehicle.java
@@ -0,0 +1,7 @@
+package OOPPolymorphism;
+
+public class Vehicle {
+    public void go(){
+        System.out.println("The vehicle begins moving");
+    }
+}
